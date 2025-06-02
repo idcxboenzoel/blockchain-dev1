5724bd26d7c81b4ffed5b7bcf0bc10604811eb35
idcxboenzoel <bonjolman@gmail.com>
2025-06-02 16:07:57 +0700

    Update server and broadcast service to use environment variables for port configuration

--- main.go
+++ main.go
@@ -85,7 +85,10 @@ func main() {
 		AllowCredentials: true,
 	}).Handler(mux)
 
-	port := "9292"
+	port := os.Getenv("SERVER_PORT")
+	if port == "" {
+		port = "9292"
+	}
 
 	server := &http.Server{
 		Addr:         listenAddr + ":" + port,
@@ -110,7 +113,10 @@ func initBroadcast() (*broadcast.BroadcastService, error) {
 	handler := &NodeHandler{}
 	bs := broadcast.NewBroadcastService(handler)
 
-	port := "9999"
+	port := os.Getenv("BROADCAST_PORT")
+	if port == "" {
+		port = "9999"
+	}
 	go func() {
 		if err := bs.Start(port); err != nil {
 			fmt.Printf("Broadcast service error: %v\n", err)
